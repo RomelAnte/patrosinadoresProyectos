5c29fee0a827dd743bc6ecab36a286a273a2b6a6
RomelAnte <rsag20092002@gmail.com>
2025-01-27 15:37:01 -0500

    Add form validation to project and sponsor management views

--- application/views/Projects/newProject.php
+++ application/views/Projects/newProject.php
@@ -1,6 +1,6 @@
 <h1 class="text-center">New Sponsor</h1>
 <br>
-<form action="<?php echo site_url('Projects/save'); ?>" method="post">
+<form action="<?php echo site_url('Projects/save'); ?>" method="post" id='newProject'>
     <label for="name">Name:</label>
     <input type="text" name="name" id="name" class='form-control'>
     <label for="description">Description:</label>
@@ -14,3 +14,50 @@
     <br>
     <input type="submit" value="Submit" class='btn btn-primary'>
 </form>
+
+<script>
+    $(document).ready(function() {
+        $('#newProject').validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 15
+                },
+                description: {
+                    required: true,
+                    minlength: 15
+                },
+                responsible: {
+                    required: true,
+                    minlength: 6
+                },
+                startDate: {
+                    required: true
+                },
+                endDate: {
+                    required: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Please enter a name",
+                    minlength: "Name must be at least 15 characters long"
+                },
+                description: {
+                    required: "Please enter a description",
+                    minlength: 15
+                },
+                responsible: {
+                    required: "Please enter a responsible",
+                    minlength: 6
+                },
+                startDate: {
+                    required: "Please enter a start date"
+                },
+                endDate: {
+                    required: "Please enter an end date"
+                }
+            }
+        });
+    });
+</script>
--- application/views/Sponsors/newSponsor.php
+++ application/views/Sponsors/newSponsor.php
@@ -1,14 +1,58 @@
 <h1 class="text-center">New Sponsor</h1>
 <br>
-<form action="<?php echo site_url('Sponsors/save'); ?>" method="post">
+<form action="<?php echo site_url('Sponsors/save'); ?>" method="post" id= "sponsorForm">
     <label for="name">Name:</label>
     <input type="text" name="name" id="name" class='form-control'>
+    <br>
     <label for="contact">Contact:</label>
     <input type="text" name="contact" id="contact" class='form-control'>
+    <br>
     <label for="email">Email:</label>
     <input type="email" name="email" id="email" class='form-control'>
+    <br>
     <label for="phone">Phone:</label>
     <input type="text" name="phone" id="phone" class='form-control'>
     <br>
+    <br>
     <input type="submit" value="Submit" class='btn btn-primary'>
 </form>
+
+<script>
+    $(document).ready(function() {
+        $('#sponsorForm').validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 10
+                },
+                contact: {
+                    required: true,
+                    minlength: 3
+                },
+                email: {
+                    required: true,
+                },
+                phone: {
+                    required: true,
+                },
+            },
+            message: {
+                name: {
+                    required: "Please enter your name",
+                    minlength: "Your name must be at least 10 characters long"
+                },
+                contact: {
+                    required: "Please enter your contact",
+                    minlength: "Your contact must be at least 3 characters long"
+                },
+                email:{
+                    required: "Please enter your email",
+                },
+                phone:{
+                    required: "Please enter your phone",
+                },
+            }    
+        })
+    });
+
+</script>
